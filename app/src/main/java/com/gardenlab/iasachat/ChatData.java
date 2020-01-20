a8b00ae4963a8b6615b2f1a6a44f59136a47d835
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-20 16:34:48 +0900

    Before Firebase

---
+++ app/src/main/java/com/gardenlab/iasachat/ChatData.java
@@ -0,0 +1,28 @@
+package com.gardenlab.iasachat;
+
+public class ChatData {
+
+    private String nick;
+    private String msg;
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    ChatData(String nick, String msg){
+        this.nick = nick;
+        this.msg = msg;
+    }
+}
--- app/src/main/java/com/gardenlab/iasachat/MainActivity.java
+++ app/src/main/java/com/gardenlab/iasachat/MainActivity.java
@@ -1,14 +1,39 @@
 package com.gardenlab.iasachat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<ChatData> chatList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.RecyclerView_chatlist);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        chatList = new ArrayList<>();
+        chatList.add(new ChatData("jw" ,"hi"));
+        mAdapter = new MyAdapter(chatList);
+        recyclerView.setAdapter(mAdapter);
     }
 }
